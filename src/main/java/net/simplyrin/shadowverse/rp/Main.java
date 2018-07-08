08f5cca2cf1a049d975a7b2b84a7b201d2b2606d
SimplyRin <simplyrin.dev@gmail.com>
2018-07-09 00:06:58 +0900

    Add files via upload (ActiveTask Mode)

--- src/main/java/net/simplyrin/shadowverse/rp/Main.java
+++ src/main/java/net/simplyrin/shadowverse/rp/Main.java
@@ -1,7 +1,12 @@
 package net.simplyrin.shadowverse.rp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.OffsetDateTime;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import com.jagrosh.discordipc.IPCClient;
 import com.jagrosh.discordipc.entities.DiscordBuild;
@@ -41,6 +46,48 @@ public class Main {
 	}
 
 	public void run() {
+		File file = new File(".");
+		System.out.println(file.getPath());
+
+		this.startTask();
+		this.addShutdownHook();
+		this.keepAlive();
+
+		System.out.println("Shadowverse IPC has ready! Have fun!");
+
+		Scanner scanner = new Scanner(System.in);
+
+		while(true) {
+			String[] args = scanner.nextLine().split(" ");
+
+			if(args[0].equalsIgnoreCase("/shutdown")) {
+				System.exit(0);
+				return;
+			}
+
+			if(args[0].equalsIgnoreCase("/details") || args[0].equalsIgnoreCase("/d")) {
+				if(args.length > 1) {
+					String details = "";
+					for(int i = 1; i < args.length; i++) {
+						details = details + args[i] + " ";
+					}
+					details.substring(0, details.length() - 1);
+					this.presence.setDetails(details);
+					this.ipcClient.sendRichPresence(this.presence.build());
+					System.out.println("Details has been updated to '" + details + "'!");
+					return;
+				}
+				System.out.println("Usage: /details <details>");
+				return;
+			}
+
+			System.out.println("Available commands");
+			System.out.println("/shutdown - Closing Shadowverse IPC");
+		}
+	}
+
+	public void connect() {
+		this.ipcClient = null;
 		this.ipcClient = new IPCClient(465472223843713024L);
 		try {
 			this.ipcClient.connect(new DiscordBuild[0]);
@@ -49,19 +96,82 @@ public class Main {
 			System.exit(0);
 			return;
 		}
-
+		this.presence = null;
 		this.presence = new RichPresence.Builder();
 		this.presence.setDetails("Playing Rank Match");
 		this.presence.setStartTimestamp(OffsetDateTime.now());
 		this.presence.setSmallImage("mini", "Shadowverse から逃げるな");
 		this.presence.setLargeImage("shadowverse", "Shadowverse");
 		this.ipcClient.sendRichPresence(this.presence.build());
+	}
 
-		this.addShutdownHook();
-		this.command();
-		this.keepAlive();
+	public void disconnect() {
+		if(this.ifNull) {
+			this.ipcClient.close();
+			this.ipcClient = null;
+		}
+	}
 
-		System.out.println("Shadowverse IPC has ready! Have fun!");
+	private boolean ifNull = false;
+	private boolean back = false;
+
+	public void startTask() {
+		ThreadPool.run(() -> {
+			while(true) {
+				boolean isShadowverse = this.isShadowverse();
+
+				if(isShadowverse) {
+					if(!this.back) {
+						System.out.println("Shadowverse が実行されています。");
+						this.connect();
+						this.back = true;
+						this.ifNull = true;
+					}
+				} else if(this.back) {
+					System.out.println("Shadowverse 以外が実行されています。");
+					this.disconnect();
+					this.back = false;
+				}
+
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public boolean isShadowverse() {
+		String title = this.getActiveWindowTitle();
+		if(title != null) {
+			if(title.equals("Shadowverse")) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public String getActiveWindowTitle() {
+		String title = null;
+
+		ProcessBuilder processBuilder = new ProcessBuilder("ActiveTask.exe");
+		Process process;
+		try {
+			process = processBuilder.start();
+		} catch (IOException e) {
+			return title;
+		}
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+		try {
+			title = bufferedReader.readLine();
+		} catch (IOException e) {
+			return title;
+		}
+
+		return title;
 	}
 
 	public void keepAlive() {
@@ -71,40 +181,6 @@ public class Main {
 					Thread.sleep(Integer.MAX_VALUE);
 				} catch (Exception e) {
 				}
-			}
-		});
-	}
-
-	public void command() {
-		ThreadPool.run(() -> {
-			Scanner scanner = new Scanner(System.in);
-
-			while(true) {
-				String[] args = scanner.nextLine().split(" ");
-
-				if(args[0].equalsIgnoreCase("/shutdown")) {
-					System.exit(0);
-					return;
-				}
-
-				if(args[0].equalsIgnoreCase("/details") || args[0].equalsIgnoreCase("/d")) {
-					if(args.length > 1) {
-						String details = "";
-						for(int i = 1; i < args.length; i++) {
-							details = details + args[i] + " ";
-						}
-						details.substring(0, details.length() - 1);
-						this.presence.setDetails(details);
-						this.ipcClient.sendRichPresence(this.presence.build());
-						System.out.println("Details has been updated to '" + details + "'!");
-						return;
-					}
-					System.out.println("Usage: /details <details>");
-					return;
-				}
-
-				System.out.println("Available commands");
-				System.out.println("/shutdown - Closing Shadowverse IPC");
 			}
 		});
 	}
